b99a70c1905551c0c49be79d4a480c54e4e15b4f
thomoncik <tjhomoncik@gmail.com>
2018-06-09 19:18:09 +0200

    Erase comment left from example

--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,3 @@
-//////////////////////////////////////////////////////////////////////////////////////////////
-//
-//  Selba Ward - Spline *EXAMPLE*
-//
-//  by Hapaxia (https://github.com/Hapaxia)
-//
-//
-//  Draw a spline in the window by moving the mouse.
-//
-//
-//    Controls:
-//
-//  Escape key          Quit
-//  Mouse movement      Draw
-//  Mouse wheel         Adjust thickness
-//  F1 key              Toggle pause
-//  Space key           Toggle bezier/linear curve mode
-//  Tab key             Toggle primitive type
-//
-//  Low frame rate is intentional so that fewer points are added to show how bezier interpolation can create curves from very few points
-//
-//  Please note that this example makes use of C++11 features
-//    and also requires the SFML library (http://www.sfml-dev.org)
-//
-//////////////////////////////////////////////////////////////////////////////////////////////
-
 #include "Simulation.h"
 
 int main() {
